fb32e7f5806f67483ea189934a6f5ca1d0e982cd
khaled taymour <khaled.taymour@gmail.com>
2020-10-02 14:10:54 +0200

    weather icons

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import * as MapApiHandler from './modules/map/mapApiHandler.js';
-import {getTemperatureByLatLng} from './modules/weather/weatherApiHandler.js';
+import { getTemperatureByLatLng } from './modules/weather/weatherApiHandler.js';
 
 const OFFCLASS = 'off';
 const ONCLASS = 'on';
 
 let weatherPointBtn = document.getElementById("weatherPoint_btn");
+let weatherImg = document.getElementById("weatherImage");
 let locationLbl = document.getElementById("location");
+let timeLbl = document.getElementById("time");
 let tempartureLbl = document.getElementById("temparture");
 let humidityLbl = document.getElementById("humidity");
 let descriptionLbl = document.getElementById("description");
@@ -23,14 +25,33 @@ weatherPointBtn.addEventListener("click", function () {
     }
 });
 
-export const handleGetTempFromLatLng = (_lat, _lng)=>{
+export const handleGetTempFromLatLng = (_lat, _lng) => {
     getTemperatureByLatLng(_lat, _lng);
 }
 
-export const showTemperature = ({...data})=>{
-    console.log(locationLbl, data);
+export const showTemperature = ({ ...data }) => {
+    console.log(data);
+    setWeatherIcon(data.current.weather[0].icon);
     locationLbl.innerText = `Location: Lat:${data.lat}, Long: ${data.lon}`;
+    let time = getHumanTime(data.current.dt);
+    timeLbl.innerText = `Time: ${time}`;
     tempartureLbl.innerText = `Temperature: ${data.current.temp} °C`;
     humidityLbl.innerText = `Humidity: ${data.current.humidity} %`;
     descriptionLbl.innerText = `${data.current.weather[0].description}`;
 };
+
+const getHumanTime = (timestamp) => {
+    var date = new Date(timestamp * 1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+
+    // Will display time in 10:30 format
+    return hours + ':' + minutes.substr(-2);
+}
+
+const setWeatherIcon = (iconCode)=>{
+    let iconURL = `http://openweathermap.org/img/w/${iconCode}.png`;
+    weatherImg.src = iconURL;
+}
--- modules/weather/weatherApiHandler.js
+++ modules/weather/weatherApiHandler.js
@@ -7,5 +7,5 @@ export const getTemperatureByLatLng = (_lat, _lng)=>{
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${_lat}&lon=${_lng}&appid=${API_KEY}&units=metric&lang=${lang}`)
     .then(res => res.json())
     .then(data => showTemperature(data))
-    .catch(err => console.log(err));
+    .catch(err => console.error(err));
 }
